4556b2ac83417248d025e1c88b9781e23e61f70b
Pooja Bhosale <bhosalepooja1415@gmail.com>
2026-06-02 18:41:49 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,12 @@
+public class Info {
+    public static void main(String[] args) {
+
+        String name = "Pooja";
+        int age = 21;
+        double cgpa = 8.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
---
+++ Math.java
@@ -0,0 +1,13 @@
+public class Math{
+	public static void main(String args[]) {
+
+		int num= 10;
+		int square= num * num;
+		int cube= num * num * num;
+		System.out.println("Number="+num);
+		System.out.println("Square="+square);
+		System.out.println("Cube="+cube);
+	}
+}
+
+		
